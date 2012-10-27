7207a83ce5886a3228cc4d90d08d046c572a178d
Rob Gough <robertgough@gmail.com>
2012-10-27 17:38:03 +0100

    Initial checking (blank rails)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Berr::Application.config.secret_token = '2e19468ce3c2cc36fbdb367a4132f963d70c82845c19eab267234f1a0ec621dab557e4e0613aeffc4ab11162f649814fa5d546c3e6cb644d343686d8adc79095'
